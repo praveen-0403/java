2ee59ce32da79a15d39903bd0d128ff9ad3a6e24
praveen-0403 <161416585+praveen-0403@users.noreply.github.com>
2024-02-27 12:31:21 +0530

    Delete java programs directory

--- java programs/Add.java
+++
@@ -1,8 +0,0 @@
-class Add{
-public static void main(String []args){
-int num1 = 213;
-int num2 = 452;
-int res= num1+num2;
-System.out.print("sum="+res);
-}
-}
--- java programs/Hightech.java
+++
@@ -1,5 +0,0 @@
-class Hightech{
-public static void main(String []args){
-System.out.print("vanakkam");
-}
-}
